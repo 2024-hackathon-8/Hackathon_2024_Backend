2b2ccb831d18f754fb57a89f1cdedec3e520d051
dkflfkd53 <b57062562@gmail.com>
2024-07-16 13:33:54 +0900

    add :: tag entity 추가

---
+++ src/main/java/com/example/hackathon_2024/domain/tag/entity/Tag.java
@@ -0,0 +1,32 @@
+package com.example.hackathon_2024.domain.tag.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "tag_name", unique = true, nullable = false)
+    private String tagName;
+
+    public Tag(String tagName) {
+        this.tagName = tagName;
+    }
+}
