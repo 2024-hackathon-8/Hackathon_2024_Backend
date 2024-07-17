f6fd4e974bdf67e4166ecb3c5ceee601e2956453
dkflfkd53 <b57062562@gmail.com>
2024-07-17 09:49:51 +0900

    delete :: 기존 board api 삭제

--- src/main/java/com/example/hackathon_2024/domain/board/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hackathon_2024.domain.board.repository;
-
-import com.example.hackathon_2024.domain.board.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
